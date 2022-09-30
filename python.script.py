8a12c137b1c3422e94a981df9038b50029587142
Connor8a <connorochoa@mail.weber.edu>
2022-09-29 18:37:24 -0600

    Finished example

--- python.script.py
+++ python.script.py
@@ -2,20 +2,13 @@
 #   - Create the following variables
 #   - my_first_name
 #       -set this equal to your first name
-from operator import methodcaller
-
-
-my_first_name = 'Connor'
-
+my_first_name = 'Scott'
 #   - my_last_name
 #       -set this equal to your last name
-my_last_name = 'Ochoa'
-
-
+my_last_name = 'Hadzik'
 #   - my_year_of_birth
-my_birth_year = 2003
-
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
+my_birth_year = 1980
 #   - current_year
 #       -set this equal to 2022
 current_year = 2022
@@ -25,28 +18,27 @@ current_year = 2022
 
 # TODO String Indexing
 #   - Print the following items (one per line) (print using variables)
-#       - first name  
+#       - first name
 #       - last name
 #       - first letter of your first name (use the +index)
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - second two letter of your last name (use the -index)
-# print (my_first_name)
-# print (my_last_name)
-# print (my_first_name[3])
-# print (my_last_name[-4])
-# print (my_first_name[0:2])
-# print (my_last_name[-3:])
+print (my_first_name)
+print (my_last_name)
+print(my_first_name[0])
+print(my_last_name[-5])
+print(my_first_name[0:2])
+print(my_last_name[-2:])
+
 
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
-# print(my_first_name, my_last_name)
-# counter = 0
-# while counter < 6:
-#     print(my_first_name)
-#     counter = counter + 1
+print(my_first_name + my_last_name)
+print(my_first_name,my_last_name)
+print((my_first_name,"\n") * 6)
 
 
 
@@ -54,28 +46,17 @@ current_year = 2022
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-birth_year_statement = "{} {} was born in {}"
+birth_year_statement = my_first_name + my_last_name + 'was born in' + my_birth_year
 
-print(birth_year_statement.format(my_first_name, my_last_name, str(my_birth_year)))
-print(birth_year_statement.format(my_first_name, my_last_name, str(my_birth_year)))
-birth_year_statement = my_first_name + ' ' + my_last_name + ' ' + str(my_birth_year) + ' ' +my_first_name + " enjoyed celebrating 2022"
-print(birth_year_statement)
+
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
 #       - possesive first name -birth year is- year of birth 
 #       - tab last name current year
-escape_example = "{}\'s birth year is {}"
-print(escape_example.format(my_first_name, my_birth_year))
 
-escape_last_name = '\t{}'
-print(escape_last_name.format(my_last_name))
 
 # TODO String methods
 #   - Print the following items (one per line) (print using variables)
 #       - first name and last name in lower case
 #       - length of last name
 #       - first name and last name all in upper case
-print(my_first_name.casefold(), my_last_name.casefold())
-print(len(my_last_name))
-print(my_first_name.upper(), my_last_name.upper())
-
