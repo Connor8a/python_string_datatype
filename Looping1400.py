3618811dff59c35b676af1f89e0a5712e971dec0
Connor8a <connorochoa@mail.weber.edu>
2022-10-06 12:20:39 -0600

    ReSaving

---
+++ Looping1400.py
@@ -0,0 +1,16 @@
+print("hello")
+print("we can get into trouble")
+
+
+"""
+for looping - loop a variable number of times 
+"""
+
+# loop 10 times
+for i in range(10):
+    print("I am inside a loop")
+
+#count cookies
+for i in range(12):
+    # I have xx cookies, BAW HAHA !!
+    print("I have", i, "cookies, BWA HA HA!!")
