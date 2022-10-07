9bf396a08ac77414be8e23f5d4de4c5ba04f226f
Connor8a <connorochoa@mail.weber.edu>
2022-10-06 18:09:43 -0600

    changes

---
+++ pythonfundamentals/numbers.py
@@ -0,0 +1,30 @@
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+import random
+
+myInteger = 1923
+secInteger = 1978
+myRandomYear = random.randint(1974, 2022)
+
+timepassed = (myRandomYear - myInteger)
+count = 0
+
+while count < 3:
+    print(timepassed)
+    count = count + 1
+
+applePrice = 1.79
+orangePrice = 1.25
+bananaPrice = 2
+bananaPriceFloat = float(bananaPrice)
+
+bill = applePrice + orangePrice + bananaPriceFloat
+print(bill)
