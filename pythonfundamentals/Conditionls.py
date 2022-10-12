65ab2322fa201fac9b1c6a173117662bcb3506c1
Connor8a <connorochoa@mail.weber.edu>
2022-10-11 18:51:28 -0600

    while loop adjustments

--- pythonfundamentals/Conditionls.py
+++ pythonfundamentals/Conditionls.py
@@ -19,19 +19,34 @@ print("")
 
 import random
 #ask the user to select the upper bound
-upper_bound = input("What is the upper bound? ")
-upper_bound = int(upper_bound)
+invalid_upper_bound = True # boolean True of Flase 
+
+while invalid_upper_bound:
+    upper_bound = input("What is the upper bound? ")
+    #check if uppoer bound is vaild returns -> string 
+    if upper_bound.isdigit():
+        upper_bound = int(upper_bound)
+        invalid_upper_bound = False
+    else:
+        print("that is not a vaild input.")
+    
+
 
 #generate a random integer starting at 1 and going to upper_bound 
 random_number = random.randint(1, upper_bound) 
 print(random_number)
 
-user_guess = None
+invalid_upper_bound = True
+user_guess = 0
+number_of_guesses = 1
 #start and loop here 
 while user_guess != random_number:
-    #ask the user to guess 
     user_guess = input("what number do you guess between 1 and " + str(upper_bound) + ": ")
+    #check if guess is a digit 
 
+    #check if uppoer bound is vaild returns -> string 
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
     #check if user guess is = to random number
 
     if int(user_guess) == random_number:
@@ -39,7 +54,10 @@ while user_guess != random_number:
         #exit the loop
     if int(user_guess) != random_number:
         print("you lose")
-print("Game Over.")
+        number_of_guesses += 1
+print(number_of_guesses)
+
+print("Game Over you took " + str(number_of_guesses) + " guesses." )
 
 # high_range = 100
 # mid_range = (high_range/2)
